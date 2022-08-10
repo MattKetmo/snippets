aa585fec371ee7716ff715b2c1b8e2e59d11c05e
Matthieu Moquet <matthieu@moquet.net>
2022-08-10 08:47:16 +0200

    add env var NEXT_PUBLIC_MY_TOKEN

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,22 +1,24 @@
 import Head from "next/head";
 import { Button } from "ui";
 
+const myToken = process.env.NEXT_PUBLIC_MY_TOKEN
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Web - Turborepo Example</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+      <main className="w-auto px-4 pt-16 pb-8 mx-auto sm:pt-24 lg:px-8">
         <h1 className="mx-auto max-w-5xl text-center text-6xl font-extrabold leading-[1.1] tracking-tighter text-white sm:text-7xl lg:text-8xl xl:text-8xl">
           Web <br className="hidden lg:block" />
-          <span className="inline-block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent">
-            Turborepo Example
+          <span className="inline-block text-transparent bg-gradient-to-r from-brandred to-brandblue bg-clip-text">
+            {myToken}
           </span>{" "}
         </h1>
-        <div className="mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8">
+        <div className="max-w-xl mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
           <Button />
         </div>
       </main>
